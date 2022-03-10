8c8b03cfe30689464740ca94cca1a88193b8c5d1
alanncalderon <alan2010c@hotmail.com>
2022-03-10 17:36:08 +0100

    exercise and examples done in class

---
+++ f_strings.py
@@ -0,0 +1,11 @@
+print("1+2 =", 1+2)
+##add F at the begging
+print(f"1+2 = {1+2}")
+name = input("What is your name?")
+print(f"Dear {name}, 1+2= {1+2}")
+a=3
+b=7
+print(f"Dear {name.capitalize()}, 1+2={b**a}")
+
+#looking at the differences between fork and code
+#control click -1
---
+++ if_example.py
@@ -0,0 +1,8 @@
+a=5
+b=5
+if a>b:
+    print("The fist number is bigger")
+elif a<b:
+    print("the fist number is smaller")
+else:
+    print("They are equal")
